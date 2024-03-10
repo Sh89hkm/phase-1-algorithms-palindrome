c94c727b40a8c0c8d3c9607ef5682b27df98438b
shvan <shvanabdulhakeem2020@gmail.com>
2024-03-10 11:12:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,81 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
-
 /* 
   Add your pseudocode here
+
+  solution 1
+  reverse the input string
+  if the reversed string is the same as the input
+  return true
+  else
+  return false
+
+  create an array from the input string
+  reverse the array
+  create a string from the reversed array
+  return the reversed string
+
+  solution 2
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+  if the letters don't match, return false
+  if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
 */
+/* solution 1 long
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
+
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+*/
+/* solution 1 short
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+*/
+
+
+// solution 2 better optimized performance
+function isPalindrome(word) {
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -18,8 +85,44 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
+/*
+  console.log("Expecting: ih");
+  console.log("=>", reverseString("hi"));
+
+  console.log("");
+
+  console.log("Expecting: tobor");
+  console.log("=>", reverseString("robot"));
+
+  console.log("");
+
+  console.log("Expecting: mom");
+  console.log("=>", reverseString("mom"));
+*/
+
 }
 
 module.exports = isPalindrome;
